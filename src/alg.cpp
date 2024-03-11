91a317d4cfa3db29417ee824c0e2813d900ea572
pomahaha <110399575+pomahaha@users.noreply.github.com>
2024-03-12 00:45:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,68 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  for (int i=2;i<value;i++){
+        if (value%i==0){
+            return false;
+        }
+        return true;
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int nomer=0;
+  int chislo=2;
+  int save;
+    while(nomer<n){
+    int schetprost=0;
+      for(int i=1;i<=chislo;i++){
+        if(chislo%i==0){
+          schetprost=schetprost+1;
+        }
+      }
+    if(schetprost<3){
+        save=chislo;
+        nomer=nomer+1;
+    }
+    chislo=chislo+1;
 }
-
+return save;
+}
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    int chislo=value+1;
+    int schet=0;
+    bool f=true;
+    while(f=true){
+        for(int i=1;i<=chislo;i++){
+            if(chislo%i==0){
+                schet=schet+1;
+            }
+        }
+        if(schet<3){
+            return chislo;
+            f=false;
+        }
+        else{
+            chislo=chislo+1;
+            schet=0;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    int sum=0;
+    int chislo=2;
+    while(chislo<hbound){
+        int schet=0;
+        for(int i=1;i<=chislo;i++){
+            if(chislo%i==0){
+                schet=schet+1;
+            }
+        }
+        if (schet<3){
+            sum=sum+chislo;
+        }
+        chislo=chislo+1;
+    }
+    return sum;
 }
