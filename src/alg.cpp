2ff64cbdabf44c51bc9c8b42066f8970745212f6
pomahaha <110399575+pomahaha@users.noreply.github.com>
2024-03-12 23:40:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,51 +3,52 @@
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
-  for (int i=2;i<value;i++){
-        if (value%i==0){
+  for (int i=2; i < value; i++) {
+        if (value%i == 0) {
             return false;
         }
     }
     return true;
 }
+
 uint64_t nPrime(uint64_t n) {
-  int nomer=0;
-  int chislo=2;
+  int nomer = 0;
+  int chislo = 2;
   int save;
-    while(nomer<n){
-    int schetprost=0;
-      for(int i=1;i<=chislo;i++){
-        if(chislo%i==0){
-          schetprost=schetprost+1;
+    while(nomer < n) {
+    int schetprost = 0;
+      for(int i=1; i <= chislo;  i++) {
+        if(chislo%i == 0) {
+          schetprost = schetprost+1;
         }
       }
-    if(schetprost<3){
-        save=chislo;
-        nomer=nomer+1;
+    if(schetprost < 3) {
+        save = chislo;
+        nomer = nomer+1;
     }
-    chislo=chislo+1;
+    chislo = chislo+1;
 }
 return save;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    int chislo=value+1;
-    int schet=0;
+    int chislo = value+1;
+    int schet = 0;
     int save;
-    bool f=true;
-    while(f==true){
-        for(int i=1;i<=chislo;i++){
-            if(chislo%i==0){
-                schet=schet+1;
+    bool f = true;
+    while(f == true) {
+        for(int i=1; i <= chislo; i++) {
+            if(chislo%i == 0) {
+                schet = schet+1;
             }
         }
-        if (schet>2){
-            schet=0;
-            chislo=chislo+1;
+        if (schet > 2) {
+            schet = 0;
+            chislo = chislo+1;
         }
         else{
-            f=false;
-            save=chislo;
+            f = false;
+            save = chislo;
         }
     }
     return save;
